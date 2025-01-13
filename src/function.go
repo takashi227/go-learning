af0dedb6efd9fd6b8bb4116e242b2f4979733e5d
takashi_suzuki <pero857942632@gmail.com>
2025-01-14 05:59:48 +0900

    function

---
+++ src/function.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// func Plus(x int, y int) int {
+// 	return x + y
+// }
+
+// 引数の型が同じ場合、省略可能
+func Plus(x, y int) int {
+	return x + y
+}
+
+func Div(x, y int) (int, int) {
+	q := x / y
+	r := x % y
+	return q, r
+}
+
+func Double(price int) (result int) {
+	result = price * 2
+	return
+}
+
+func NoReturn() {
+	fmt.Println("No return")
+}
+
+func main() {
+	var i int = Plus(1, 2)
+	fmt.Println(i)
+
+	var q, r int = Div(9, 2)
+	fmt.Println(q, r)
+
+	// 戻り値の一部を無視する
+	q, _ = Div(20, 3)
+	fmt.Println(q)
+
+	fmt.Println(Double(100))
+
+	NoReturn()
+}

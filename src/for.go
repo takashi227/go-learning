d71bf060a687964a6260953ba94f01d25482fbec
takashi_suzuki <pero857942632@gmail.com>
2025-01-18 05:24:54 +0900

    for

---
+++ src/for.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Loop:
+	// 	for {
+	// 		for {
+	// 			for {
+	// 				fmt.Println("START")
+	// 				break Loop
+	// 			}
+	// 			fmt.Println("処理をしない")
+	// 		}
+	// 		fmt.Println("処理をしない")
+	// 	}
+	// 	fmt.Println("END")
+Loop:
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if j > 1 {
+				continue Loop
+			}
+			fmt.Println(i, j, i*j)
+		}
+		fmt.Println("処理をしない")
+	}
+
+}

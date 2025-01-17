909d2cbcd9bf44645c7b812dceacb1979abf5bb3
takashi_suzuki <pero857942632@gmail.com>
2025-01-18 05:46:16 +0900

    goroutin

---
+++ src/goroutin.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sub() {
+	for {
+		fmt.Println("Sub loop")
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func main() {
+	go sub()
+	go sub()
+
+	for {
+		fmt.Println("Main loop")
+		time.Sleep(200 * time.Millisecond)
+	}
+
+}

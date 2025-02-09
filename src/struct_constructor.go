a3667a77be9f810058676c7f8f06ef31fe709d16
takashi_suzuki <pero857942632@gmail.com>
2025-02-09 10:14:47 +0900

    sturct constructor

---
+++ src/struct_constructor.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func NewUser(name string, age int) *User {
+	return &User{
+		Name: name,
+		Age:  age,
+	}
+}
+
+func main() {
+	user1 := NewUser("User1", 10)
+	fmt.Println(user1)
+	fmt.Println(*user1)
+}

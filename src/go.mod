ed9a5aa035e729d59500ce9940aa7b6db3421af9
takashi_suzuki <pero857942632@gmail.com>
2025-02-14 06:06:27 +0900

    test

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module example.com/go-learning
+
+go 1.23.4
---
+++ src/sample/test.go
@@ -0,0 +1,14 @@
+package sample
+
+func IsOne(i int) bool {
+	if i == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+// func main() {
+// 	fmt.Println(IsOne(1))
+// 	fmt.Println(IsOne(2))
+// }
